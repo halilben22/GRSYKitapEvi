a745ef285091fde44d78f1167d790ef3243d4939
Halil <halilattar01@gmail.com>
2023-09-23 11:03:35 +0300

    -

--- GRSYKitapevi_DataAccess/Data/ApplicationDbContext.cs
+++ GRSYKitapevi_DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GRSYKitapEvi_Models.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +14,12 @@ namespace GRSYKitapevi_DataAccess.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base
             (options)
         {
-                
+          
+            
 
         }
+
+        public DbSet<Kategori> Kategoriler { get; set; }
 
     }
 }
---
+++ GRSYKitapevi_DataAccess/Migrations/20230923080007_EkleKategoriTablosuDB.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GRSYKitapevi_DataAccess.Migrations
+{
+    public partial class EkleKategoriTablosuDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kategoriler",
+                columns: table => new
+                {
+                    KategoriId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KategoriAd = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kategoriler", x => x.KategoriId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kategoriler");
+        }
+    }
+}
